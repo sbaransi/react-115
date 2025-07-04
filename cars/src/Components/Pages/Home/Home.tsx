4dbdeea59c6081ec5a0220c8dd44d2c1261d1566
Gal Amouyal <galamouy@Gals-MBP.lan>
2025-07-04 17:48:56 +0300

    a

--- cars/src/Components/Pages/Home/Home.tsx
+++ cars/src/Components/Pages/Home/Home.tsx
@@ -6,6 +6,7 @@ const URL = "http://localhost:2200";
 
 export function Home(): JSX.Element {
   const [isSalesLoading, setIsSalesLoading] = useState<boolean>(false);
+  const [currentMonth, setCurrentMonth] = useState("");
   const [sales, setSales] = useState<
     {
       description: string;
@@ -21,12 +22,12 @@ export function Home(): JSX.Element {
   useEffect(() => {
     getSales();
     return () => {};
-  }, []);
+  }, [currentMonth]);
 
   async function getSales() {
     try {
       setIsSalesLoading(true);
-      const result = await axios.get(`${URL}/sales`);
+      const result = await axios.get(`${URL}/sales?input=${currentMonth}`);
       setSales(result?.data?.data);
     } catch (error) {
       alert("Something went wrong");
@@ -39,9 +40,30 @@ export function Home(): JSX.Element {
     <div className="Home">
       <h1> Home</h1>
 
-      <button>This Month Sales</button>
-      <button>All Sales</button>
-      
+      <button
+        onClick={() => {
+          setCurrentMonth((new Date().getMonth() + 1).toString());
+        }}
+      >
+        This Month Sales
+      </button>
+
+      <button
+        onClick={() => {
+          setCurrentMonth((new Date().getMonth() + 2).toString());
+        }}
+      >
+        Next Month Sales
+      </button>
+
+      <button
+        onClick={() => {
+          setCurrentMonth("");
+        }}
+      >
+        All Sales
+      </button>
+
       {isSalesLoading ? (
         <h2>Loading...</h2>
       ) : (
